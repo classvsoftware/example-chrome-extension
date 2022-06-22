8d62c5af9c7a560794b6d81ba664bb0396c8a3c7
Matt Frisbie <msfrisbie@gmail.com>
2022-06-22 10:25:42 -0500

    toast

---
+++ components/modify-toolbar-icon/modify-toolbar-icon.js
@@ -0,0 +1,37 @@
+import { initializeBoilerplate, showToast } from "/scripts/shared.js";
+
+initializeBoilerplate();
+
+document.querySelector("#toolbar-image").addEventListener("click", () => {
+  const randomHexColor =
+    "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+  const canvas = new OffscreenCanvas(16, 16);
+  const context = canvas.getContext("2d");
+  context.clearRect(0, 0, 16, 16);
+  context.fillStyle = randomHexColor;
+  context.fillRect(0, 0, 16, 16);
+  const imageData = context.getImageData(0, 0, 16, 16);
+  chrome.action.setIcon({ imageData: imageData }, () => {
+    showToast({
+      title: "Updated Extension Icon",
+      body: `Set tooltip icon to "${randomHexColor}"`,
+    });
+  });
+});
+
+document.querySelector("#tooltip-btn").addEventListener("click", () => {
+  const title = document.querySelector("#tooltip").value;
+
+  chrome.action.setTitle(
+    {
+      title,
+    },
+    () => {
+      showToast({
+        title: "Updated Extension Tooltip",
+        body: `Set tooltip to "${title}"`,
+      });
+    }
+  );
+});
--- scripts/shared.js
+++ scripts/shared.js
@@ -50,3 +50,52 @@ export async function initializeBoilerplate() {
     window.location.reload();
   });
 }
+
+let toastContainer = null;
+
+export function showToast({ title, body, subtitle = "" }) {
+  if (!toastContainer) {
+    toastContainer = document.createElement("div");
+    toastContainer.classList.add(
+      "absolute",
+      "top-0",
+      "right-0",
+      "p-4",
+      "flex",
+      "flex-col",
+      "gap-4",
+      "items-stretch",
+      "z-50"
+    );
+    document.body.appendChild(toastContainer);
+  }
+
+  const toast = document.createElement("div");
+
+  toastContainer.appendChild(toast);
+
+  toast.innerHTML = `
+<div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+  <div class="toast-header">
+    <img src="/icons/codesearch_16x16.png" class="rounded mr-2" />
+    <strong class="mr-auto">${title}</strong>
+    <small>${subtitle}</small>
+    <button
+      type="button"
+      class="ml-2 mb-1 close"
+      data-dismiss="toast"
+      aria-label="Close"
+    >
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  <div class="toast-body">${body}</div>
+</div>
+  `;
+
+  new bootstrap.Toast(toast.querySelector(".toast")).show();
+
+  setTimeout(() => {
+    toastContainer.removeChild(toast);
+  }, 5000);
+}
